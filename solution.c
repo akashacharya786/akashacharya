cc27187a88228df04d4323012b6298781d20f9e5
akashacharya786 <53473436+akashacharya786@users.noreply.github.com>
2019-08-13 15:57:23 +0530

    Create solution.c

---
+++ solution.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int a[10].b[10],n;
+printf("enter the number of ele\n"0;
+scanf("%d",&n);
+printf("enter 1 array ele\n");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+printf("enter 2 array ele\n");
+for(i=0;i<n;i++)
+scanf("%d",&b[i]);
+for(i=0;i<n;i++)
+if(a[i]!=b[i])
+{
+found=1;
+}
+if(found==0)
+printf("arrays are equal\n");
+else
+printf("array are different\n");
+}
+}
